a5003a9d3a82006e3850ba085c78516cdb14682a
PRAVEEN KUMAR <117474739+AchintyaG@users.noreply.github.com>
2023-09-14 15:49:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+const encoder = new TextEncoder();
+  const bytes = encoder.encode(str);
+  return bytes.length;
 };
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
